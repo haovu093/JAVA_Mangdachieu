fa528727a33fd6d3a7634e0eeaa8a0ad4d2b0438
HaoVT <haovt7@onemount.com>
2021-05-26 16:28:15 +0700

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String args[]) {
 //        Mangmotchieu m1 = new Mangmotchieu();
 //        m1.timSNT();
-        TimSoChan s1= new TimSoChan();
-        s1.timSoChan();
+//        TimSoChan s1= new TimSoChan();
+//        s1.timSoChan();
 //
 //        Mangdachieu d1 = new Mangdachieu();
 //        d1.nhapMang();
 //
 //        BT9 bai9 = new BT9();
 //        bai9.soDaoNguoc();
-//        TamGiac tg1 = new TamGiac();
-//        tg1.inTamgiac();
+        TamGiac tg1 = new TamGiac();
+        tg1.inTamgiac();
 //        HinhCN cn1= new HinhCN();
 //        cn1.inHCN();
     }
--- src/TamGiac.java
+++ src/TamGiac.java
@@ -11,9 +11,8 @@ public class TamGiac {
         } while (height <= 0);
 
         for (int i = 1; i <= height; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("* ");
-            }
+            for (int j = 1; j <= height - i; j++) System.out.print(" ");
+            for (int j = 1; j <= 2 * i - 1; j++) System.out.print("*");
             System.out.println();
         }
 
